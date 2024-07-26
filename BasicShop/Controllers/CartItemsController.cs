101c1f660037b70b4c7d18552a42eb4ab20d442f
Abdelrahman <abdelrahman.abdalla@stemredsea.moe.edu.eg>
2024-07-26 19:08:46 +0300

    implement Swager

---
+++ BasicShop/Controllers/CartItemsController.cs
@@ -0,0 +1,82 @@
+﻿using BasicShop.Data;
+using BasicShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartItemsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public CartItemsController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CartItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CartItem>>> GetCartItems()
+        {
+            return await _context.CartItems.ToListAsync();
+        }
+
+        // GET: api/CartItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CartItem>> GetCartItem(int id)
+        {
+            var cartItem = await _context.CartItems.FindAsync(id);
+
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            return cartItem;
+        }
+
+        // POST: api/CartItems
+        [HttpPost]
+        public async Task<ActionResult<CartItem>> PostCartItem(CartItem cartItem)
+        {
+            _context.CartItems.Add(cartItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCartItem", new { id = cartItem.Id }, cartItem);
+        }
+
+        // PUT: api/CartItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCartItem(int id, CartItem cartItem)
+        {
+            if (id != cartItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cartItem).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/CartItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCartItem(int id)
+        {
+            var cartItem = await _context.CartItems.FindAsync(id);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.CartItems.Remove(cartItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- BasicShop/Program.cs
+++ BasicShop/Program.cs
@@ -1,13 +1,32 @@
+using BasicShop.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ShopContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
+
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BasicShop", Version = "v1" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseAuthorization();
 
 app.MapControllers();
